db033216fba77d1c22e8efd3a89cfa552241a5f9
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-10 21:35:47 +0900

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -11,6 +11,11 @@ install_docker() {
 	  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   $RUN apt-get update -y
   $RUN apt-get install docker-ce docker-ce-cli containerd.io -y
+  
+  # To resolve 'permission denied while trying to connect to the Docker daemon socket at\
+  #	unix:///var/run/docker.sock: Get "http://%2Fvar%2Frun%2Fdocker.sock/v1.24/containers/json":\
+  #	dial unix /var/run/docker.sock: connect: permission denied' error
+  $RUN usermod -a -G docker $USER
 }
 
 install_docker
