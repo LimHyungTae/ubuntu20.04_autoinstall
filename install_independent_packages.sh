654d08677094f933342f4b23bdcd42b5b61e45fb
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-10 19:47:00 +0900

    Update install_independent_packages.sh

--- install_independent_packages.sh
+++ install_independent_packages.sh
@@ -10,13 +10,17 @@ user_can_sudo() {
   ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
 }
 
+# check whether 'sudo' is enabled or not
+RUN=$(user_can_sudo && echo "sudo" || echo "command")
+
 install_neovim() {
-  RUN=$(user_can_sudo && echo "sudo" || echo "command")
+  
   $RUN add-apt-repository ppa:neovim-ppa/stable 
   $RUN apt-get update -y
   $RUN apt-get install neovim -y
 }
 
+install_chrome()
 main() {
   # Parse arguments
   while [ $# -gt 0 ]; do
@@ -31,16 +35,25 @@ main() {
   
   RUN=$(user_can_sudo && echo "sudo" || echo "command")
   
-  $RUN apt-get update
-  $RUN apt-get install -y git curl wget vim
+  # $RUN apt-get update
+  # $RUN apt-get install -y git curl wget vim
   
   # chrome
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  $RUN dpkg -i google-chrome-stable_current_amd64.deb
+  # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  # $RUN dpkg -i google-chrome-stable_current_amd64.deb
   
   # NeoVIM
-  install_neovim
+  # install_neovim
+  
+  # Resolve the font issue of `agnoster` theme of zsh. Although you don't use zsh, it's totally fine
+  # Easy way: If your are using Ubuntu >= 16.04 you can simply run: 
+  $RUN apt-get install fonts-powerline -y
+  
+  # Set minimal C++ example 
+  $RUN apt install cmake libeigen3-dev libboost-all-dev -y
 
+  # For playing mkv file
+  $RUN apt install ubuntu-restricted-extras mpv -y
 
 }
 
