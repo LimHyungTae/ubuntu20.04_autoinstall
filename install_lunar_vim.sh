90d64aef3d599db804ca20f3220b6b097605b602
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-11 01:01:25 +0900

    Update install_lunar_vim.sh

--- install_lunar_vim.sh
+++ install_lunar_vim.sh
@@ -7,3 +7,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install Lunar Vim
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+
+# Nerd Font for kitti icons
+git clone https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts && ./install.sh
