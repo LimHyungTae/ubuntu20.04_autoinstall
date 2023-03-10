75d505205f8a9ac1d549f5528b39bcadbdb776f2
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-11 00:29:31 +0900

    Create install_lunarvim.sh

---
+++ install_lunarvim.sh
@@ -0,0 +1,5 @@
+# To install Lunar Vim, Neo-Vim > 0.8 is required
+wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb
+sudo apt install ./nvim-linux64.deb
+
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
