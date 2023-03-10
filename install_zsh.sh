b8c291de204cd302618c41579c58dfb647a131c1
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-10 21:21:22 +0900

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo apt-get install zsh -y
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Resolve the font issue of `agnoster` theme of zsh. Although you don't use zsh, it's totally fine
+# Easy way: If your are using Ubuntu >= 16.04 you can simply run: 
+# NOTE: It requires agreeing with something, so automatic installation may ask you to agree to something
+sudo apt-get install fonts-powerline -y
