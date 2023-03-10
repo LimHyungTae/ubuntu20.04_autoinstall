e3ccdb5725ed119a870dc7cfa42972c5e6fcde6a
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-10 21:33:26 +0900

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -9,8 +9,8 @@ install_docker() {
   echo \
 	  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 	  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  $RUN apt-get update
-  $RUN apt-get install docker-ce docker-ce-cli containerd.io
+  $RUN apt-get update -y
+  $RUN apt-get install docker-ce docker-ce-cli containerd.io -y
 }
 
 install_docker
