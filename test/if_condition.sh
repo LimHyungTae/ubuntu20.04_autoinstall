df5028fcb2351f21a945c0a53d18757147e11961
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-10 21:50:38 +0900

    Update if_condition.sh

--- test/if_condition.sh
+++ test/if_condition.sh
@@ -11,13 +11,17 @@ user_can_sudo() {
 }
 
 main() {
-  
+  echo "$*"
+  echo "$*"
+  echo "$*"
   if [ "$*" == "bash" ]; then
-    echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-    source ~/.bashrc
+    # echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+    # source ~/.bashrc
+    echo "'Bash' is coming!"
   elif [ "$*" = "zsh" ]; then
-    echo "source /opt/ros/noetic/setup.zsh" >> ~/.zshrc
-    source ~/.zshrc  
+    # echo "source /opt/ros/noetic/setup.zsh" >> ~/.zshrc
+    # source ~/.zshrc  
+    echo "'zsh' is coming!"
   else
     echo "[Warning] Wrong input coming! Maybe adding auto-source failed"
   fi 
