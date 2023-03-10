ac2567b52d79c0d6dc430e6a83b91943dac67902
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-11 00:40:29 +0900

    Update install_lunar_vim.sh

--- install_lunar_vim.sh
+++ install_lunar_vim.sh
@@ -2,4 +2,8 @@
 wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb
 sudo apt install ./nvim-linux64.deb
 
+#Install cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install Lunar Vim
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
