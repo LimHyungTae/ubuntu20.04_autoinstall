7ac4e383ea6b0b53e1311e673f4d4125f9a849a6
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-03-12 03:34:00 +0900

    Update install_catkin_tools.sh

--- install_catkin_tools.sh
+++ install_catkin_tools.sh
@@ -3,4 +3,4 @@ sudo sh \
         > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install python3-catkin-tools
+sudo apt-get install python3-catkin-tools -y
