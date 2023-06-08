bbf878090dd05b9728930672e58cf42bef563ae2
Hyungtae Lim <35317311+LimHyungTae@users.noreply.github.com>
2023-06-08 23:57:03 +0900

    Create install_zsh_plugins.sh

---
+++ install_zsh_plugins.sh
@@ -0,0 +1,11 @@
+################## Plugins ################## 
+
+# fasd
+sudo add-apt-repository ppa:aacebedo/fasd
+sudo apt-get update -y
+sudo apt-get install fasd -y
+
+# zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
